caa8084e2a4d66913b07bbfc094f704049ef67c6
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2023-12-10 23:15:15 +0000

    fixed tsp fitness2

--- TSPComponent/Classes/Fitness.py
+++ TSPComponent/Classes/Fitness.py
@@ -1,26 +1,18 @@
 import numpy as np
 
 
-class Mutation:
-    def __init__(self, child1, child2, mutation_rate):
-        self.child1 = child1
-        self.child2 = child2
-        self.mutation_rate = mutation_rate
+class Fitness:
+    def __init__(self, distance_matrix):
+        self.distance_matrix = distance_matrix
 
-    def swap_mutation(self, num_swaps):
+    def calc_fitness(self, chromosome: np.ndarray) -> float:
         """
-        Performs swap mutation
-        :return: child1, child2: tuple[ndarray, ndarray]
+        Method to calculate distance for each individual solution
+        :param chromosome: ndarray
+        :return: sum of the distance: int
         """
-        def multi_swap(arr, num_swaps):
-            for _ in range(num_swaps):
-                idx1, idx2 = np.random.choice(len(arr), size=2, replace=False)
-                arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
-
-        rng = np.random.default_rng()
-        if rng.random() < self.mutation_rate:
-            multi_swap(self.child1, num_swaps)
-        if rng.random() < self.mutation_rate:
-            multi_swap(self.child2, num_swaps)
-
-        return self.child1, self.child2
+        return sum(
+            [
+                self.distance_matrix[chromosome[i], chromosome[i + 1]] for i in range(len(chromosome) - 1)
+            ]
+        ) + self.distance_matrix[chromosome[-1], chromosome[0]]
